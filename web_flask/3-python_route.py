4674a590ce7047898106b064895ee66b19e7d9bb
EBUKA EMMANUEL EJIE <tripleeoliver@gmail.com>
2023-04-22 16:12:55 -0700

    Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""WebFlask module"""
+from flask import Flask
+from os import environ
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """Display Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """Display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Display text"""
+    real_text = text.replace('_', ' ')
+    return 'C {}'.format(real_text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    """Display text"""
+    real_text = text.replace('_', ' ')
+    return 'Python {}'.format(real_text)
+
+
+if __name__ == '__main__':
+    environ['FLASK_APP'] = __file__
+    app.run(host='0.0.0.0', port=5000)
