de3bf875a813941dd89ecc721ac915e83c88c688
EBUKA EMMANUEL EJIE <tripleeoliver@gmail.com>
2023-04-23 02:18:58 -0700

    HBNB filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""WebFlask application module"""
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def show_filters():
+    """Shows filters"""
+    from models.state import State
+    from models.amenity import Amenity
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    return render_template("10-hbnb_filters.html", states=states, amenities=amenities)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
